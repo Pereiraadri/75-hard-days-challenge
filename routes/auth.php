5017a6443410e2ec3cf12f435e5c4958ed5dc9ea
Pereiraadri <pereiraadrien3@gmail.com>
2026-05-14 18:30:09 +0200

    redirect to login

--- routes/auth.php
+++ routes/auth.php
@@ -12,6 +12,10 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('login');
+    });
+
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
